73a3dd6e458288eca3d388ddb973c54be75e4e95
Dominik Sekielewski <fran07@gmail.com>
2025-09-29 20:27:58 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myFavouritFotballClub = "ChelseaLondyn";
+        int stadiumCapacity = 41837;
+        double yearOfFoundation = 1905;
+        String clubColors = "blue";
+        boolean isChelseaTheBestEnglishClub = true;
+        int numberOfChamionshipTitles = 6;
+        System.out.println(isChelseaTheBestEnglishClub);
+    }
+}
