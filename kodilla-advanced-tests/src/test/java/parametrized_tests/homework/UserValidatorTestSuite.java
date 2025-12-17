ccf2d5c2e0d7d87bf9edb1ad60a64a70fae6261d
Dominik Sekielewski <fran07@gmail.com>
2025-12-17 17:07:57 +0100

    Initial version

---
+++ kodilla-advanced-tests/src/test/java/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -0,0 +1,23 @@
+package parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserValidatorTestSuite {
+
+    private UserValidator validator = new UserValidator();
+
+    @ParameterizedTest
+    @CsvSource({"andrzej@andrzej.pl, true", "janusz.karol@gmail.com.pl, true", "eugeniusz@gov.pl, true", "jan@edmund, false", "karol@andrzej., false", "janusz.pl, false", "@krzysztof.emil, false"})
+    void shouldValidateEmailsCorrectly(String email, boolean expected) {
+        assertEquals(expected, validator.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'Jan_123', true", "'Jan.Kowalski', true", "'Jak-Kowalski', true", "'Jan123', true", "'JK', false", "'J', false", "'jk', false", "'Jan@Kowalski', false", "'Jan#Kowalski', false", "'', false"})
+    void shouldReturnExpectedResultForUsernameValidation(String username, boolean expected) {
+        assertEquals(expected, validator.validateUsername(username));
+    }
+}
