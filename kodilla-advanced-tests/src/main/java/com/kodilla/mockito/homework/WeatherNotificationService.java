6422666dbe38cb5a4c9116e4e8e5d576a3d1a25b
Dominik Sekielewski <fran07@gmail.com>
2026-03-13 14:20:15 +0100

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
@@ -7,26 +7,39 @@ import java.util.Set;
 
 public class WeatherNotificationService {
 
-    private Map<Location, Set<User>> locationSubscribers = new HashMap<>();
+    private final Map<Location, Set<User>> locationSubscribers = new HashMap<>();
 
     public void addSubscriber(User user, Location location) {
+
+        if (user == null || location == null) {
+            throw new IllegalArgumentException("User and Location are null");
+        }
         Set<User> users = locationSubscribers.computeIfAbsent(location, k -> new HashSet<>());
         users.add(user);
     }
 
     public void removeSubscriber(User user, Location location) {
+        if (user == null || location == null) {
+            throw new IllegalArgumentException("User and Location are null");
+        }
         if (locationSubscribers.containsKey(location)) {
             locationSubscribers.get(location).remove(user);
         }
     }
 
     public void removeAllSubscriptions(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("User is null");
+        }
         for (Set<User> users : locationSubscribers.values()) {
             users.remove(user);
         }
     }
 
     public void sendNotificationToLocation(String message, Location location) {
+        if (location == null) {
+            throw new IllegalArgumentException("Location is null");
+        }
         if (locationSubscribers.containsKey(location)) {
             for (User user : locationSubscribers.get(location)) {
                 user.receive(message);
@@ -34,7 +47,7 @@ public class WeatherNotificationService {
         }
     }
 
-    public void sendNotificationToAll(String message) {
+    public void sendNotificationToAll(String message) { // zbiór Userów jest stworzony po to, aby każdy użytkownik otzymał wiadomość tylko raz (nawet jeżeli subskrybuje kilka lokalizacji)
         Set<User> allUsers = new HashSet<>();
 
         for (Set<User> users : locationSubscribers.values()) {
@@ -47,6 +60,9 @@ public class WeatherNotificationService {
     }
 
     public void removeLocation(Location location) {
+        if (location == null) {
+            throw new IllegalArgumentException("Location is null");
+        }
         locationSubscribers.remove(location);
     }
 }
